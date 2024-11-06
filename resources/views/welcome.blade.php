9b670fd2e45f9d471aa6c9db83428b9d8d7769b4
Adriano Jr <adrianojunr@gmail.com>
2024-11-06 14:21:36 -0300

    Add vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LockPassManager</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-secondary text-light">
-    <h1 class="text-center">TESTE</h1>
+    <div>
+        <h1 class="text-center display-1">ALERTA</h1>
+        <ul class="text-center">
+            <li>Começar a escrever quando o prototipo estiver pronto</li>
+            <li>Manter um código organizado</li>
+            <li>Usar algum linter para validar o código</li>
+            <li>Alterar cores do framework bootstrap, para usar as cores do projeto</li>
+            <li class="display-6 text-danger">Atualizar o estado do trabalho no Trello</li>
+        </li>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
