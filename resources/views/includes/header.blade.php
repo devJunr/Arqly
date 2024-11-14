90e2578ee518425eb36fa24019347c78e4ac45fb
ETE PC-07 <etePc@EteLimoeiro.comgit config --global user.email etePc@EteLimoeiro.com>
2024-11-13 21:11:25 -0300

    Add footer, header, carrossel

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar bg-dark text-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <h1 class="text-white"><i class="bi bi-key text-white"></i> LockPassManager - @yield('title') </h1>
+    </a>
+  </div>
+</nav>
---
+++ resources/views/senhas.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title','Senhas')
+
+@section('content')
+
+<h1>Senhas</h1>
+
+@endsection
