fb9e6e0bc10e10e5f0ebd4bfc64d6a36a24760cb
ETE PC-07 <etePc@EteLimoeiro.comgit config --global user.email etePc@EteLimoeiro.com>
2024-11-26 20:29:51 -0300

    Editar criar senhas & adicionar migration senhas

--- resources/views/newPass.blade.php
+++ resources/views/newPass.blade.php
@@ -2,10 +2,11 @@
 @section('title','Nova Senha')
 @section('content')
 
+<div class="d-flex justify-content-center align-items-center flex-column">
 <h1 class="display-4 text-center p-4">Nova Senha</h1>
 <p class="text-center">Adicione sua nova conta ou gere uma senha para ter mais segurança</p>
-<div class="container text-white bg-dark m-4 d-flex align-itens-center justify-content-center flex-column p-4 text-white">
-    <div class="box">
+<div class="container text-white bg-dark m-4 d-flex p-4 text-white">
+    <div class="box d-flex flex-column gap-2 align-items-center justify-content-center">
         <label for="qtde">Digite a quantidade de caracteres</label>
         <input class="text-black" type="text" name="qtde" id="qtde" placeholder="Digite um número">
         <button type="submit" id="btn-enviar" class="btn btn-primary">Gerar senha</button>
@@ -13,12 +14,6 @@
         <button type="submit" class="btn btn-danger" id="btn-copiar" disable>Copiar</button>
     </div>
 </div>
+</div>
 
-<script>
-    $('#btn-enviar').click(() => {
-        $.ajax(
-            url:
-        )
-    })    
-</script>
 @endsection
