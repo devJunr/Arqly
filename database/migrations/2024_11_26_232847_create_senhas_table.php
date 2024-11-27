075f5e081ecaf5b7722eefad45c67c4de1e66554
ETE PC-07 <etePc@EteLimoeiro.comgit config --global user.email etePc@EteLimoeiro.com>
2024-11-26 21:06:23 -0300

    Add cor de fundo

--- database/migrations/2024_11_26_232847_create_senhas_table.php
+++ database/migrations/2024_11_26_232847_create_senhas_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('senhas', function (Blueprint $table) {
             $table->id();
+            $table->string("nomeServico");
+            $table->string("senhaServico");
             $table->timestamps();
         });
     }
