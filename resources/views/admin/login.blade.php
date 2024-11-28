fbc09c2bb2d3ce2fa7e5403d5450a200b3754dfe
ETE PC-07 <etePc@EteLimoeiro.comgit config --global user.email etePc@EteLimoeiro.com>
2024-11-28 20:33:23 -0300

    Removido style css

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div class="container d-flex flex-column justify-content-center align-items-center gap-3 p-5 mt-5 mb-5 bg-dark rounded">
-    <h1>Faça o seu login</h1>
+    <h1 class="display-1 p-1">Login</h1>
     <form class="d-flex flex-column gap-1" action="" method="get">
         <div class="d-flex flex-column gap-2" style="width: 20rem; height: 4rem">
             <input class="text-white bg-dark border border-danger" type="email" id="email" placeholder="Usuário"/>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,4 @@
-<link rel="stylesheet" href="../style/animation.css">
-<nav class="d-flex pt-3 pb-3 gap-5 bg-black text-danger justify-content-center">
+<nav class="d-flex pt-3 bg-black text-danger justify-content-center">
   <a class="animacaoOp" href="/">Home</a>
   <a class="animacaoOp" href="/senhas">Senhas</a>
   <a class="animacaoOp" href="/newPass">Adicionar</a>
