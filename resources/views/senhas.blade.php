576d2be9194036ac9a09ab033635ed2c734dd78f
ETE PC-07 <etePc@EteLimoeiro.comgit config --global user.email etePc@EteLimoeiro.com>
2024-12-04 20:14:07 -0300

    Add livewire

--- resources/views/senhas.blade.php
+++ resources/views/senhas.blade.php
@@ -7,7 +7,8 @@
 <div class="container">
     <section class="text-center display-6 p-5">
         <h2>Senhas</h2>
-
+    </section>
+    
     <section class="newPassShow" style="display: none;">
         <iframe src="/newPass" width="100%" height="60%">
     </section>
